14fbcb3ff219baf0bebf06896e4d7e7743940cb6
Lorenzo Young <lorenzo.ivan.young@gmail.com>
2026-01-12 04:57:35 -0700

    Working on the CSS of the Request Form

--- frontend/src/RequestForm.jsx
+++ frontend/src/RequestForm.jsx
@@ -1,3 +1,4 @@
+import "./RequestForm.css";
 const RequestForm = () => {
   const onSubmit = (e) => {
     e.preventDefault();
@@ -8,232 +9,229 @@ const RequestForm = () => {
   };
 
   return (
-    <div>
+    <div id="requestformcontainer">
       <h1>Access Request Form</h1>
       <div>
         <form onSubmit={onSubmit} id="requestform">
           <div>
             <h3>Requestor Information</h3>
           </div>
-          <div>
-            <label htmlFor="date">Date: </label>
-            <input type="date" id="date" name="date" required class="datebox" />
-            <br />
-            <label htmlFor="requestno">Request Number: </label>
+          <div id="requestorinfo" className="section">
+            <label htmlFor="date" className="requestor">
+              Date:
+            </label>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              required
+              className="datebox requestor"
+            />
+            <label htmlFor="requestno" className="requestor">
+              Request Number:
+            </label>
             <input
               type="text"
               id="requestno"
               name="requestno"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes requestor"
             />
-            <br />
-            <label htmlFor="requestorfname">Requestor First Name: </label>
+            <label htmlFor="requestorfname" className="requestor">
+              Requestor First Name:
+            </label>
             <input
               type="text"
               id="requestorfname"
               name="requestorfname"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes requestor requestorname"
             />
-            <br />
-            <label htmlFor="requestorlname">Requestor Last Name: </label>
+            <label htmlFor="requestorlname" className="requestor">
+              Requestor Last Name:
+            </label>
             <input
               type="text"
               id="requestorlname"
               name="requestorlname"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes requestor requestorname"
             />
-            <br />
-            <label htmlFor="email">Email Address: </label>
+            <label htmlFor="email" className="requestor">
+              Email Address:
+            </label>
             <input
               type="text"
               id="email"
               name="email"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes requestor"
             />
-            <br />
-            <label htmlFor="contactno">Contact Number: </label>
+            <label htmlFor="contactno" className="requestor">
+              Contact Number:
+            </label>
             <input
               type="text"
               id="contactno"
               name="contactno"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes requestor"
             />
-            <br />
           </div>
           <div>
             <h3>Employee Information</h3>
           </div>
-          <div>
+          <div id="employeeinfo" className="section">
             <label htmlFor="empNo">Employee Number: </label>
             <input
               type="text"
               id="empNo"
               name="empNo"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes"
             />
-            <br />
             <label htmlFor="jobrole">Job Role: </label>
             <input
               type="text"
               id="jobrole"
               name="jobrole"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes"
             />
-            <br />
             <label htmlFor="firstname">First Name: </label>
             <input
               type="text"
               id="firstname"
               name="firstname"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes"
             />
-            <br />
             <label htmlFor="lastname">Last Name: </label>
             <input
               type="text"
               id="lastname"
               name="lastname"
               required
-              maxlength="80"
-              class="textboxes"
+              maxLength="80"
+              className="textboxes"
             />
-            <br />
           </div>
           <div>
             <h3>System Access Type</h3>
           </div>
-          <div>
+          <div id="systeminfo" className="section">
             <label htmlFor="lan">LAN: </label>
-            <input type="checkbox" id="lan" name="lan" class="checkboxes" />
-            <br />
+            <input type="checkbox" id="lan" name="lan" className="checkboxes" />
             <label htmlFor="dccs">DCCS Access: </label>
-            <input type="checkbox" id="dccs" name="dccs" class="checkboxes" />
-            <br />
+            <input
+              type="checkbox"
+              id="dccs"
+              name="dccs"
+              className="checkboxes"
+            />
             <label htmlFor="yardsmart">Yardsmart:</label>
             <input
               type="checkbox"
               id="yardsmart"
               name="yardsmart"
-              class="checkboxes"
+              className="checkboxes"
             />
-            <br />
             <label htmlFor="ims">IMS: </label>
-            <input type="checkbox" id="ims" name="ims" class="checkboxes" />
-            <br />
+            <input type="checkbox" id="ims" name="ims" className="checkboxes" />
             <label htmlFor="imi">IMI: </label>
-            <input type="checkbox" id="imi" name="imi" class="checkboxes" />
-            <br />
+            <input type="checkbox" id="imi" name="imi" className="checkboxes" />
             <label htmlFor="aom">AOM: </label>
-            <input type="checkbox" id="aom" name="aom" class="checkboxes" />
-            <br />
+            <input type="checkbox" id="aom" name="aom" className="checkboxes" />
             <label htmlFor="gdrive">Group Drive:</label>
             <input
               type="checkbox"
               id="gdrive"
               name="gdrive"
-              class="checkboxes"
+              className="checkboxes"
             />
-            <br />
             <label htmlFor="dlist">Email Distribution List: </label>
-            <input type="checkbox" id="dlist" name="dlist" class="checkboxes" />
-            <br />
+            <input
+              type="checkbox"
+              id="dlist"
+              name="dlist"
+              className="checkboxes"
+            />
             <label htmlFor="tds">TDS: </label>
-            <input type="checkbox" id="tds" name="tds" class="checkboxes" />
-            <br />
+            <input type="checkbox" id="tds" name="tds" className="checkboxes" />
             <label htmlFor="jda">JDA: </label>
-            <input type="checkbox" id="jda" name="jda" class="checkboxes" />
-            <br />
+            <input type="checkbox" id="jda" name="jda" className="checkboxes" />
             <label htmlFor="mytr">MyTR: </label>
-            <input type="checkbox" id="mytr" name="mytr" class="checkboxes" />
-            <br />
+            <input
+              type="checkbox"
+              id="mytr"
+              name="mytr"
+              className="checkboxes"
+            />
             <label htmlFor="vms">VMS: </label>
-            <input type="checkbox" id="vms" name="vms" class="checkboxes" />
-            <br />
+            <input type="checkbox" id="vms" name="vms" className="checkboxes" />
           </div>
           <div>
             <h3>Other Information:</h3>
           </div>
-          <div>
-            <div>
-              <label htmlFor="gdrivedetails">Group Drive Details: </label>
-              <input
-                type="text"
-                id="gdrivedetails"
-                name="gdrivedetails"
-                required
-                maxlength="240"
-                class="textboxes"
-              />
-              <br />
-              <label htmlFor="dlistdetailed">Distribution List Details: </label>
-              <input
-                type="text"
-                id="dlistdetailed"
-                name="dlistdetailed"
-                required
-                maxlength="240"
-                class="textboxes"
-              />
-              <br />
-              <label htmlFor="dccsdetails">DCCS Access Details: </label>
-              <input
-                type="text"
-                id="dccsdetails"
-                name="dccsdetails"
-                required
-                maxlength="240"
-                class="textboxes"
-              />
-              <br />
-              <label htmlFor="yardsmartdetails">Yardsmart Details: </label>
-              <input
-                type="text"
-                id="yardsmartdetails"
-                name="yardsmartdetails"
-                required
-                maxlength="80"
-                class="textboxes"
-              />
-              <br />
-              <label htmlFor="imsdetails">IMS Details: </label>
-              <input
-                type="text"
-                id="imsdetails"
-                name="imsdetails"
-                required
-                maxlength="80"
-                class="textboxes"
-              />
-              <br />
-              <label htmlFor="otherdetails">Others: </label>
-              <input
-                type="text"
-                id="otherdetails"
-                name="otherdetails"
-                required
-                maxlength="80"
-                class="textboxes"
-              />
-              <br />
-            </div>
+          <div id="otherinfo" className="section">
+            <label htmlFor="gdrivedetails">Group Drive Details: </label>
+            <textarea
+              id="gdrivedetails"
+              name="gdrivedetails"
+              required
+              maxLength="240"
+              className="textboxes otherinfo"
+            />
+            <label htmlFor="dlistdetailed">Distribution List Details: </label>
+            <textarea
+              id="dlistdetailed"
+              name="dlistdetailed"
+              required
+              maxLength="240"
+              className="textboxes otherinfo"
+            />
+            <label htmlFor="dccsdetails">DCCS Access Details: </label>
+            <textarea
+              id="dccsdetails"
+              name="dccsdetails"
+              required
+              maxLength="240"
+              className="textboxes otherinfo"
+            />
+            <label htmlFor="yardsmartdetails">Yardsmart Details: </label>
+            <textarea
+              id="yardsmartdetails"
+              name="yardsmartdetails"
+              required
+              maxLength="80"
+              className="textboxes otherinfo"
+            />
+            <label htmlFor="imsdetails">IMS Details: </label>
+            <textarea
+              id="imsdetails"
+              name="imsdetails"
+              required
+              maxLength="80"
+              className="textboxes otherinfo"
+            />
+            <label htmlFor="otherdetails">Others: </label>
+            <textarea
+              id="otherdetails"
+              name="otherdetails"
+              required
+              maxLength="80"
+              className="textboxes otherinfo"
+            />
           </div>
-          <div>
+          <div className="formbuttons">
             <button type="reset" value="Reset" id="btnreset">
               Reset
             </button>
